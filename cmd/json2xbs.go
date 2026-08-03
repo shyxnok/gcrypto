8242b1512805caf72752d9381551a45b320376be
bgcode <violetcode@qq.com>
2026-08-04 01:36:14 +0800

    up

--- cmd/json2xbs.go
+++ cmd/json2xbs.go
@@ -1,6 +1,3 @@
-/*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
@@ -16,16 +13,15 @@ var (
 	xbsOutPath   string
 )
 
-// json2xbsCmd represents the json2xbs command
 var json2xbsCmd = &cobra.Command{
-	Use:   "json2xbs",
+	Use:   "j2x",
 	Short: "json to xbs",
 	Run: func(cmd *cobra.Command, args []string) {
 		buffer, err := xbs.LoadFile(jsonFilePath)
 		cobra.CheckErr(err)
 		xbsBuffer, err := xbs.Json2XBS(buffer)
 		cobra.CheckErr(err)
-		os.MkdirAll(filepath.Dir(jsonOutPath), os.ModePerm)
+		os.MkdirAll(filepath.Dir(xbsOutPath), os.ModePerm)
 		err = os.WriteFile(xbsOutPath, xbsBuffer, 0644)
 		cobra.CheckErr(err)
 	},
@@ -33,16 +29,6 @@ var json2xbsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(json2xbsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// json2xbsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// json2xbsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	json2xbsCmd.Flags().StringVarP(&jsonFilePath, "json", "i", "", "json file path")
 	json2xbsCmd.Flags().StringVarP(&xbsOutPath, "xbs", "o", "", "xbs output path")
 	json2xbsCmd.MarkFlagRequired("json")
--- cmd/xbs2json.go
+++ cmd/xbs2json.go
@@ -18,7 +18,7 @@ var (
 
 // xbs2jsonCmd represents the xbs2json command
 var xbs2jsonCmd = &cobra.Command{
-	Use:   "xbs2json",
+	Use:   "x2j",
 	Short: "xbs to json",
 	Run: func(cmd *cobra.Command, args []string) {
 		buffer, err := xbs.LoadFile(xbsFilePath)
@@ -33,18 +33,8 @@ var xbs2jsonCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(xbs2jsonCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// xbs2jsonCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// xbs2jsonCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	xbs2jsonCmd.Flags().StringVarP(&xbsFilePath, "xbs", "i", "", "xbs file path")
-	xbs2jsonCmd.Flags().StringVarP(&jsonOutPath, "json", "o", "", "xbs output path")
+	xbs2jsonCmd.Flags().StringVarP(&jsonOutPath, "json", "o", "", "json output path")
 	xbs2jsonCmd.MarkFlagRequired("xbs")
 	xbs2jsonCmd.MarkFlagRequired("json")
 }
